764258d46ea95c4100756564f1325a913fc4fb1c
Dmitriy <dr.trem86@gmail.com>
2018-11-12 12:52:04 +0200

    add init == []

--- lib/factory.rb
+++ lib/factory.rb
@@ -14,3 +14,50 @@
 # - to_a
 # - values_at
 # - ==, eql?
+
+class Factory
+  class << self
+    def new(*key, &block)
+      const_set(key.shift.capitalize, class_new(*key, &block)) if key.first.is_a?(String)
+      class_new(*key, &block)
+    end
+
+    def class_new(*key, &block)
+      Class.new do
+        attr_accessor(*key)
+
+        define_method :initialize do |*value|
+          raise ArgumentError, 'ArgumentError' if key.count != value.count
+          key.zip(value).each {|key, value| send("#{key}=", value)}
+        end
+
+        class_eval(&block) if block_given?
+
+        def map_instance_variables
+          instance_variables.map { |variable| instance_variable_get(variable) }
+        end
+
+        def ==(obj)
+          self.class == obj.class && self.map_instance_variables == obj.map_instance_variables
+        end
+
+        def [](key)
+          (key.is_a? Numeric) ? map_instance_variables[key] : instance_variable_get("@#{key}")
+        end
+
+        def []=(key, value)
+          instance_variable_set("@#{key}", value)
+        end
+
+        def dig(*args)
+          
+        end
+
+
+
+        class_eval(&block) if block_given?
+      end
+    end
+  end    
+end
+
